a84aa9e643fae6a7278d5b987800d430e583dbcd
steven-rivera <83838429+steven-rivera@users.noreply.github.com>
2026-04-22 14:23:23 -0700

    added run script

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+
+outputName="clox"
+
+# -g: Embed debug symbols into the binary
+# -I<dir>: Search <dir> when resolving `#include "..."`
+# -W<option>:
+#    - all: Show all warnings
+#    - error: Any warnings results in compile error
+
+gcc -g -Isrc/headers -Wall -Werror ./src/*.c -o "${outputName}" || exit
+"./${outputName}"
+rm "./${outputName}"
